cc64f88e765557af42084e7d9a0d591e8c846233
Jordan Johnson <jordaninjohnson@gmail.com>
2020-05-03 21:07:15 -0600

    User Input Saved

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+inquirer.registerPrompt('recursive', require('inquirer-recursive'));
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,6 +14,106 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// recursive inquirer
+inquirer.prompt([
+    {
+        name: 'start',
+        message: 'Lets Build an Engineering Team!',
+        default: '',
+    },
+    {
+        name: 'managerName',
+        message: "Manager Name: ",
+        default: 'bob',
+    },
+    {
+        name: 'id',
+        message: 'Team member ID#: ',
+        default: '1',
+    },
+    {
+        name: 'email',
+        message: 'Email: ',
+        default: 'hi@bye.com',
+    },
+    {
+        name: 'officeNumber',
+        message: 'Office #: ',
+        default: '123-456-7890',
+    },
+    {
+    type: 'recursive',
+    message: 'Add a new Engineer?',
+    name: 'engineer',
+    prompts: [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is engineer\'s name?',
+            validate: function (value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'name is required';
+            }
+        }, {
+            type: 'input',
+            name: 'id',
+            message: 'ID#: ',
+            validate: function (value) {
+                var digitsOnly = /\d+/;
+                if (digitsOnly.test(value)) { return true; }
+                return 'Invalid age! Must be a number genius!';
+            }
+        }, {
+            name: 'email',
+            message: 'Email: ',
+            default: 'hi@bye.com',
+        }, {
+            name: 'github',
+            message: 'GitHub: ',
+            default: 'Username',
+        },
+    ]
+    },
+    {
+    type: 'recursive',
+    message: 'Add a new Intern?',
+    name: 'intern',
+    prompts: [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is intern\'s name?',
+            validate: function (value) {
+                if ((/.+/).test(value)) { return true; }
+                return 'name is required';
+            }
+        }, {
+            type: 'input',
+            name: 'id',
+            message: 'ID#: ',
+            validate: function (value) {
+                var digitsOnly = /\d+/;
+                if (digitsOnly.test(value)) { return true; }
+                return 'Invalid age! Must be a number genius!';
+            }
+        }, {
+            name: 'email',
+            message: 'Email: ',
+            default: 'hi@bye.com',
+        }, {
+            name: 'school',
+            message: 'Name of School: ',
+            default: 'University of Utah',
+        },
+    ]
+    },
+    ]).then(function (answers) {
+        console.info('Answers:', answers);
+        //console.log(answers.engineer);
+        //console.log(answers.intern);
+
+    });
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
